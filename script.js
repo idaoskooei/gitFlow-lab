2a5046ede9943e663e17266b2807bdf1da000d16
mickey-vee <mvoong27@gmail.com>
2024-07-24 12:17:32 -0400

    Updated styles and scripts

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ const poems = [
     author: "Ernie",
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
-  },  
+  },
+  {
+    author: "Mickey",
+    poem: "Roses are red \n Violets are blue \n I hope that your stomach \n Isn't rumbling too",
+    image: "hungry.jpeg",
+  },
 ];
 
 let displayPoems = (array) => {
