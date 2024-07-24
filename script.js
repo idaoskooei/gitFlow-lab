ad5920fe623f30cf7dabc5f6f541d827e9115434
idaoskooei <idaoskooei@gmail.com>
2024-07-24 12:16:49 -0400

    changes made by ida

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "ida",
+    poem: "Roses are red \n Violets are blue \n Code can be sweet \n When written by you",
+    image: "vscode.jpeg",
+  },
 ];
 
 let displayPoems = (array) => {
